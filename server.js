9b1b57815e07896dd3ba37a95a8e1600bf2ee7d7
dhartley <dani_hartley@msn.com>
2021-06-01 23:18:38 -0500

    created queries for Read, Delete, and create

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
+const { brotliDecompress } = require('zlib');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -19,10 +20,38 @@ const db = mysql.createConnection({
 console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM  candidates WHERE id =?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
